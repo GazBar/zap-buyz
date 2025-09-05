ffa7c74b6c12e99a571e17228c51459231180954
GazBar <garethbarkley@gmail.com>
2025-09-05 14:29:16 +0100

    Update index.js

--- seven-17-store/src/index.js
+++ seven-17-store/src/index.js
@@ -6,7 +6,6 @@ import App from './App';
 import { CartProvider } from './CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -16,4 +15,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
